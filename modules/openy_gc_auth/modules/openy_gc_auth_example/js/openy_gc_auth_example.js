2eb4a0e2edc1d2e53e059b051fdfc32da7a0e8de
Alexey <alexey.drupal@gmail.com>
2024-01-11 12:53:58 +0100

    [Ds-1300] Fix deprecation code (once and cookie) in js files

--- modules/openy_gc_auth/modules/openy_gc_auth_example/js/openy_gc_auth_example.js
+++ modules/openy_gc_auth/modules/openy_gc_auth_example/js/openy_gc_auth_example.js
@@ -2,12 +2,13 @@
  * @file
  * JavaScript for openy_gc_auth_example.
  */
-(function ($, Drupal) {
+(function ($, Drupal, once) {
 
   Drupal.behaviors.openy_gc_auth_example = {
     attach: function(context, settings) {
-      $('input[data-autosubmit=1]', context).once('openy_gc_auth_example').form().submit();
+      $(once('openy_gc_auth-data-autosubmit', 'input[data-autosubmit=1]'))
+        .form().submit();
     }
   };
 
-})(jQuery, Drupal);
+})(jQuery, Drupal, once);
